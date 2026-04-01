dc38015905b9e75032f4bd6efd0980ee02f9b66f
xRiot45 <tomasalberto527@gmail.com>
2026-04-01 10:26:51 +0700

    refactor: add option multipart form data for axios

--- configs/http/axios.ts
+++ configs/http/axios.ts
@@ -1,48 +1,44 @@
 import axios, { AxiosHeaders, type InternalAxiosRequestConfig } from "axios"
+import { getAccessToken } from "../token-storage"
 
 const DEFAULT_TIMEOUT = 15000
 
 function getBaseURL(): string {
-  return process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000"
-}
-
-function getAccessToken(): string | null {
-  if (typeof window === "undefined") {
-    return null
-  }
-
-  return localStorage.getItem("accessToken")
+    return process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000"
 }
 
 export const axiosInstance = axios.create({
-  baseURL: getBaseURL(),
-  timeout: DEFAULT_TIMEOUT,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
+    baseURL: getBaseURL(),
+    timeout: DEFAULT_TIMEOUT,
+    headers: {
+        Accept: "application/json",
+    },
 })
 
 axiosInstance.interceptors.request.use(
-  (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-    const token = getAccessToken()
+    (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+        const token = getAccessToken()
 
-    if (token) {
-      if (!config.headers) {
-        config.headers = new AxiosHeaders()
-      }
+        if (!config.headers) {
+            config.headers = new AxiosHeaders()
+        }
 
-      config.headers.set("Authorization", `Bearer ${token}`)
-    }
+        if (token) {
+            config.headers.set("Authorization", `Bearer ${token}`)
+        }
 
-    return config
-  },
-  (error: unknown) => Promise.reject(error)
+        if (config.data instanceof FormData) {
+            config.headers.set("Content-Type", "multipart/form-data")
+        } else {
+            config.headers.set("Content-Type", "application/json")
+        }
+
+        return config
+    },
+    (error: unknown) => Promise.reject(error)
 )
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error: unknown) => {
-    return Promise.reject(error)
-  }
+    (response) => response,
+    (error: unknown) => Promise.reject(error)
 )
